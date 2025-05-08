b53ff9604b61cf6c1abd07868ee69eb505ab964f
T333333-xtz <1505312983@qq.com>
2025-05-08 19:38:24 +0800

    Update server.py
    try block and loop processing

--- server.py
+++ server.py
@@ -17,3 +17,8 @@ def handle_client(client_socket):
     global tuple_space, operation_count, error_count
     global client_count
     client_count += 1
+try:
+        while True:
+            data = client_socket.recv(1024).decode()
+            if not data:
+                break
