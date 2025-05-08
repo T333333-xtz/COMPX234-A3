edc4570a067fdf75229d6d572300d580bad52440
T333333-xtz <1505312983@qq.com>
2025-05-08 21:29:18 +0800

    Update client.py
    create send_request function.the function is to format the client's request and send it to the server, which then receives the server's response and returns

--- client.py
+++ client.py
@@ -1,3 +1,11 @@
 #client.py
 import socket
 import sys
+
+
+def send_request(client_socket, request):
+    size = len(request)
+    formatted_request = f"{size:03d}{request}"
+    client_socket.send(formatted_request.encode())
+    response = client_socket.recv(1024).decode()
+    return response
